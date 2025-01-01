d954ee61f54203867402a865e198ca19823caecb
boiledegg <tjrwns2163@naver.com>
2025-01-01 21:03:18 +0900

    [ADD/#3] 자주 사용되는 확장함수 및 클래스 추가

---
+++ app/src/main/java/com/napzak/market/core/common/extension/Context.kt
@@ -0,0 +1,13 @@
+package com.napzak.market.core.common.extension
+
+import android.content.Context
+import android.os.Message
+import android.widget.Toast
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.showLongToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}

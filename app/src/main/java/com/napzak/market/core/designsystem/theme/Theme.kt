f5f4274daaf4b3e83f00bf0ef69d38931e058ac9
Jong <jm991014@gmail.com>
2025-01-05 08:12:38 +0900

    [Add/#9] Theme 내 Color, Typo 설정

--- app/src/main/java/com/napzak/market/core/designsystem/theme/Theme.kt
+++ app/src/main/java/com/napzak/market/core/designsystem/theme/Theme.kt
@@ -8,18 +8,41 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
 
+object NapzakMarketTheme {
+    val colors: NapzakMarketColors
+        @Composable
+        get() = localNapzakMarketColors.current
+    val typography: NapzakMarketTypography
+        @Composable
+        get() = localNapzakMarketTypography.current
+}
+
+@Composable
+fun ProvideNapzakMarketColorsAndTypography(
+    colors: NapzakMarketColors,
+    typography: NapzakMarketTypography,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        localNapzakMarketColors provides colors,
+        localNapzakMarketTypography provides typography,
+        content = content
+    )
+}
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Purple10,
+    secondary = Purple20,
+    tertiary = Purple30
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Gray50,
+    secondary = Gray100,
+    tertiary = Gray200
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -39,19 +62,22 @@ fun NapzakMarketTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    ProvideNapzakMarketColorsAndTypography(
+        colors = defaultNapzakMarketColors,
+        typography = defaultNapzakMarketTypography
+    ) {
+        val colorScheme = when {
+            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                val context = LocalContext.current
+                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            }
+
+            darkTheme -> DarkColorScheme
+            else -> LightColorScheme
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        MaterialTheme(
+            content = content
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }

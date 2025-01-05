911325bae5b0ea08e8fe81b3c5fca1a46b9d1dbf
Jong <jm991014@gmail.com>
2025-01-05 17:39:44 +0900

    [Add/#9] PR 코드 리뷰 반영
    - @ReadOnlyComposable 주석
    - 불필요한 ColorScheme 제거

--- app/src/main/java/com/napzak/market/core/designsystem/theme/Theme.kt
+++ app/src/main/java/com/napzak/market/core/designsystem/theme/Theme.kt
@@ -1,22 +1,18 @@
 package com.napzak.market.core.designsystem.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.ReadOnlyComposable
 
 object NapzakMarketTheme {
     val colors: NapzakMarketColors
         @Composable
+        @ReadOnlyComposable
         get() = localNapzakMarketColors.current
     val typography: NapzakMarketTypography
         @Composable
+        @ReadOnlyComposable
         get() = localNapzakMarketTypography.current
 }
 
@@ -33,49 +29,14 @@ fun ProvideNapzakMarketColorsAndTypography(
     )
 }
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple10,
-    secondary = Purple20,
-    tertiary = Purple30
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Gray50,
-    secondary = Gray100,
-    tertiary = Gray200
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
-
 @Composable
 fun NapzakMarketTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     ProvideNapzakMarketColorsAndTypography(
         colors = defaultNapzakMarketColors,
         typography = defaultNapzakMarketTypography
     ) {
-        val colorScheme = when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
-        }
-
         MaterialTheme(
             content = content
         )

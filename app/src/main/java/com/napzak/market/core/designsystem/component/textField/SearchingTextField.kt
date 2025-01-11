0d0dab9b22adaa1e88c1fa62bdd5268b915c96d9
chrin05 <rinring105@gmail.com>
2025-01-11 23:48:40 +0900

    [UI/#12] 검색창 컴포넌트 구현

---
+++ app/src/main/java/com/napzak/market/core/designsystem/component/textField/SearchingTextField.kt
@@ -0,0 +1,117 @@
+package com.napzak.market.core.designsystem.component.textField
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.R
+import com.napzak.market.core.common.extension.noRippleClickable
+
+@Composable
+fun SearchingTextField(
+    placeholder: String,
+    value: String,
+    onTextChange: (String) -> Unit,
+    onSearchButtonClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var searchText by remember { mutableStateOf(value) }
+    val focusManager = LocalFocusManager.current
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = NapzakMarketTheme.colors.gray100,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        BasicTextField(
+            value = searchText,
+            onValueChange = {
+                searchText = it
+                onTextChange(searchText)
+            },
+            modifier = Modifier.weight(1f),
+            textStyle = NapzakMarketTheme.typography.bodySemi14,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchButtonClick()
+                    focusManager.clearFocus()
+                }
+            ),
+            decorationBox = { innerTextField ->
+                if (searchText.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        style = NapzakMarketTheme.typography.bodySemi14,
+                        color = NapzakMarketTheme.colors.gray400
+                    )
+                }
+                innerTextField()
+            }
+        )
+
+        if (searchText.isNotEmpty()) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_x_circle_16px),
+                contentDescription = stringResource(id = R.string.x_circle_button),
+                tint = Color.Unspecified,
+                modifier = Modifier.noRippleClickable {
+                    searchText = ""
+                    onTextChange(searchText)
+                }
+            )
+            Spacer(Modifier.width(4.dp))
+        }
+
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_search_24px),
+            contentDescription = stringResource(id = R.string.search_button),
+            tint = Color.Unspecified,
+            modifier = Modifier.noRippleClickable {
+                focusManager.clearFocus()
+                onSearchButtonClick()
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SearchingTextFieldPreview(modifier: Modifier = Modifier) {
+    SearchingTextField(
+        placeholder = "어떤 아이템을 찾고 계신가요?",
+        value = "",
+        onTextChange = { },
+        onSearchButtonClick = { },
+        modifier = modifier
+    )
+}

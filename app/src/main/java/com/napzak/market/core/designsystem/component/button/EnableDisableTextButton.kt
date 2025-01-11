71afe230b02b2cc3d15a2489141d9b37c4277809
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-12 00:47:23 +0900

    [UI/#13] EnableDisableTextButton 구현

---
+++ app/src/main/java/com/napzak/market/core/designsystem/component/button/EnableDisableTextButton.kt
@@ -0,0 +1,65 @@
+package com.napzak.market.core.designsystem.component.button
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+
+/**
+ * 활성화/비활성화 버튼 컴포넌트
+ *
+ * @param text 버튼에 표시될 텍스트
+ * @param isEnabled 버튼 활성화 여부
+ * @param onClick 버튼 클릭 시 수행할 동작
+ */
+
+@Composable
+fun EnableDisableTextButton(
+    text: String,
+    isEnabled: Boolean,
+    onClick: () -> Unit
+) {
+    CommonButton(
+        text = text,
+        onClick = { if (isEnabled) onClick() },
+        isEnabled = isEnabled,
+        width = 335.dp,
+        height = 52.dp,
+        shape = RoundedCornerShape(12.dp),
+        textStyle = NapzakMarketTheme.typography.bodyBold16,
+        backgroundColor = NapzakMarketTheme.colors.purple30,
+        contentColor = NapzakMarketTheme.colors.white
+    )
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 640)
+@Composable
+fun EnableDisableTextButtonPreview() {
+    NapzakMarketTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            EnableDisableTextButton(
+                text = "선택 완료! 시작하기",
+                isEnabled = true,
+                onClick = { /* Preview Action */ }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            EnableDisableTextButton(
+                text = "선택 완료! 시작하기",
+                isEnabled = false,
+                onClick = { /* Preview Action */ }
+            )
+        }
+    }
+}

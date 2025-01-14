a78643c2377761a242717de930f36b6555a5ddb7
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-14 12:45:35 +0900

    [MOD/#13] Ripple 효과 제거

--- app/src/main/java/com/napzak/market/core/designsystem/component/button/CommonButton.kt
+++ app/src/main/java/com/napzak/market/core/designsystem/component/button/CommonButton.kt
@@ -1,6 +1,5 @@
 package com.napzak.market.core.designsystem.component.button
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
@@ -8,7 +7,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import com.napzak.market.core.common.util.NoRippleInteractionSource

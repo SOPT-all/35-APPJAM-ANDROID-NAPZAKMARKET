7e988298496b80b06652ee53eb9bfa79ee050c9f
chrin05 <rinring105@gmail.com>
2025-01-14 16:38:31 +0900

    [UI/#21] 탐색탭 기본 틀 구현

--- app/src/main/java/com/napzak/market/presentation/explore/ExploreRoute.kt
+++ app/src/main/java/com/napzak/market/presentation/explore/ExploreRoute.kt
@@ -1,12 +1,82 @@
 package com.napzak.market.presentation.explore
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.napzak.market.core.common.state.UiState
+import com.napzak.market.domain.explore.model.ProductItem
+import com.napzak.market.presentation.explore.state.ExploreUiState
 
 @Composable
-fun ExploreRoute(modifier: Modifier = Modifier) {
+fun ExploreRoute(
+    modifier: Modifier = Modifier,
+    viewModel: ExploreViewModel = viewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    LaunchedEffect(uiState) {
+        viewModel.getExploreProductInformation()
+    }
+
+    ExploreScreen(
+        modifier = modifier,
+        uiState = uiState,
+    )
+}
+
+@Composable
+fun ExploreScreen(
+    modifier: Modifier = Modifier,
+    uiState: ExploreUiState
+) {
+    when (uiState.loadState) {
+        is UiState.Loading -> {
+            TODO("Loading 화면")
+        }
+
+        is UiState.Empty -> {
+            TODO("Empty 화면")
+        }
+
+        is UiState.Failure -> {
+            TODO("Failure 화면")
+        }
+
+        is UiState.Success -> {
+            with(uiState.loadState.data) {
+                ExploreSuccessScreen(
+                    modifier = modifier,
+                    tradeType = uiState.tradeType,
+                    genreList = uiState.genreList,
+                    isOnSale = uiState.isOnSale,
+                    isUnopened = uiState.isUnopened,
+                    productList = uiState.loadState.data.productList
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ExploreSuccessScreen(
+    modifier: Modifier = Modifier,
+    tradeType: String,
+    genreList: List<String>,
+    isOnSale: Boolean,
+    isUnopened: Boolean,
+    productList: List<ProductItem>
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+    }
 }
 
 @Preview
--- app/src/main/java/com/napzak/market/presentation/explore/ExploreViewModel.kt
+++ app/src/main/java/com/napzak/market/presentation/explore/ExploreViewModel.kt
@@ -1,4 +1,89 @@
 package com.napzak.market.presentation.explore
 
-class ExploreViewModel {
+import androidx.lifecycle.ViewModel
+import com.napzak.market.core.common.state.UiState
+import com.napzak.market.domain.explore.model.ProductItem
+import com.napzak.market.presentation.explore.state.ExploreProductInformation
+import com.napzak.market.presentation.explore.state.ExploreUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class ExploreViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(ExploreUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun getExploreProductInformation() {
+        TODO("추후 서버 통신 연결")
+        updateLoadState(
+            loadState = UiState.Success(
+                ExploreProductInformation(
+                    listOf(
+                        ProductItem(
+                            productId = 201,
+                            genreName = "짱구",
+                            productName = "피규어",
+                            photo = "http://example.com/photo3.jpg",
+                            price = 120000,
+                            uploadTime = "3일",
+                            isLiked = true,
+                            tradeType = "SELL",
+                            tradeStatus = "BEFORE_TRADE",
+                        ),
+                        ProductItem(
+                            productId = 201,
+                            genreName = "짱구",
+                            productName = "피규어",
+                            photo = "http://example.com/photo3.jpg",
+                            price = 120000,
+                            uploadTime = "3일",
+                            isLiked = true,
+                            tradeType = "SELL",
+                            tradeStatus = "BEFORE_TRADE",
+                        ),
+                        ProductItem(
+                            productId = 201,
+                            genreName = "짱구",
+                            productName = "피규어",
+                            photo = "http://example.com/photo3.jpg",
+                            price = 120000,
+                            uploadTime = "3일",
+                            isLiked = true,
+                            tradeType = "SELL",
+                            tradeStatus = "BEFORE_TRADE",
+                        ),
+                        ProductItem(
+                            productId = 201,
+                            genreName = "짱구",
+                            productName = "피규어",
+                            photo = "http://example.com/photo3.jpg",
+                            price = 120000,
+                            uploadTime = "3일",
+                            isLiked = true,
+                            tradeType = "SELL",
+                            tradeStatus = "BEFORE_TRADE",
+                        ),
+                        ProductItem(
+                            productId = 201,
+                            genreName = "짱구",
+                            productName = "피규어",
+                            photo = "http://example.com/photo3.jpg",
+                            price = 120000,
+                            uploadTime = "3일",
+                            isLiked = true,
+                            tradeType = "SELL",
+                            tradeStatus = "BEFORE_TRADE",
+                        ),
+                    )
+                )
+            )
+        )
+    }
+
+    private fun updateLoadState(loadState: UiState<ExploreProductInformation>) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                loadState = loadState
+            )
+        }
 }

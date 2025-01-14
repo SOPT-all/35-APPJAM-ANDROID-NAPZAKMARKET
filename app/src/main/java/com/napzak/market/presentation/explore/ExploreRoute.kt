a6600a33e2f25b86a5e0f8f2eb615e68a76a160a
chrin05 <rinring105@gmail.com>
2025-01-14 15:42:52 +0900

    [UI/#12] 탐색탭 뷰, 뷰모델 파일 생성

---
+++ app/src/main/java/com/napzak/market/presentation/explore/ExploreRoute.kt
@@ -0,0 +1,16 @@
+package com.napzak.market.presentation.explore
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ExploreRoute(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun ExploreRoutePreview(modifier: Modifier = Modifier) {
+    ExploreRoute(modifier = modifier)
+}
---
+++ app/src/main/java/com/napzak/market/presentation/explore/ExploreViewModel.kt
@@ -0,0 +1,4 @@
+package com.napzak.market.presentation.explore
+
+class ExploreViewModel {
+}

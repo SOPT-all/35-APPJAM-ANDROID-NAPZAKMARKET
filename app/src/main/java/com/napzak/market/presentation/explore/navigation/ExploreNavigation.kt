1996285fc18e7c168cba0f36f46477f2fa156629
chrin05 <rinring105@gmail.com>
2025-01-14 19:11:13 +0900

    [FEAT/#21] 탐색탭 네비게이션 연결

---
+++ app/src/main/java/com/napzak/market/presentation/explore/navigation/ExploreNavigation.kt
@@ -0,0 +1,26 @@
+package com.napzak.market.presentation.explore.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.napzak.market.core.common.navigation.MainTabRoute
+import com.napzak.market.presentation.explore.ExploreRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToExplore(navOptions: NavOptions? = null) = navigate(Explore, navOptions)
+
+fun NavGraphBuilder.exploreGraph(
+    modifier: Modifier = Modifier
+) {
+    composable<Explore> {
+        ExploreRoute(
+            modifier = modifier,
+            onSearchBoxClick = { },
+        )
+    }
+}
+
+@Serializable
+data object Explore : MainTabRoute
--- app/src/main/java/com/napzak/market/presentation/main/MainScreen.kt
+++ app/src/main/java/com/napzak/market/presentation/main/MainScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import com.napzak.market.presentation.dummy.navigation.dummyGraph
+import com.napzak.market.presentation.explore.navigation.exploreGraph
 import com.napzak.market.presentation.main.component.MainBottomBar
 import com.napzak.market.presentation.main.component.MainRegisterDialog
 import com.napzak.market.presentation.main.type.MainTab
@@ -72,5 +73,6 @@ private fun MainNavHost(
         startDestination = navigator.startDestination
     ) {
         dummyGraph(modifier = modifier)
+        exploreGraph(modifier = modifier)
     }
 }

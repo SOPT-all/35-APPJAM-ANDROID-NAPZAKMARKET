49b36d81aebba9c1c25e78c40159d3d65b97c1c4
Jong <jm991014@gmail.com>
2025-01-14 22:57:36 +0900

    [UI/#15] RegistrationPlainTextField 구현

---
+++ app/src/main/java/com/napzak/market/presentation/registration/component/RegistrationPlainTextField.kt
@@ -0,0 +1,75 @@
+package com.napzak.market.presentation.registration.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+
+/**
+ * Registration plain text field
+ *
+ * 판매/구매 등록 View에서 사용되는 일반 TextField
+ * isTitle 변수로 한 줄 혹은 여러 줄 작성 가능
+ *
+ * @param text
+ * @param placeholder
+ * @param onTextChange
+ * @param modifier
+ * @param isTitle 제목일 경우, 한 줄, 그 외에는 여러 줄
+ */
+
+@Composable
+fun RegistrationPlainTextField(
+    text: String,
+    placeholder: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    isTitle: Boolean = true,
+) {
+    BasicTextField(
+        modifier = modifier
+        .fillMaxWidth()
+        .background(NapzakMarketTheme.colors.white, RoundedCornerShape(12.dp))
+        .border(1.dp, NapzakMarketTheme.colors.gray200, RoundedCornerShape(12.dp))
+        .padding(vertical = 11.dp, horizontal = 14.dp),
+        value = text,
+        textStyle = NapzakMarketTheme.typography.bodyMedium14.copy(
+            color = NapzakMarketTheme.colors.gray900
+        ),
+        onValueChange = onTextChange,
+        singleLine = isTitle,
+        decorationBox = { innerTextField ->
+            if (text.isEmpty()) {
+                Text(
+                    text = placeholder,
+                    style = NapzakMarketTheme.typography.bodyMedium14,
+                    color = NapzakMarketTheme.colors.gray400
+                )
+            }
+            innerTextField()
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun RegistrationBaseTextFieldPreview() {
+    NapzakMarketTheme {
+        val text = remember { mutableStateOf("") }
+        RegistrationPlainTextField(
+            text = text.value,
+            placeholder = "제목을 작성해주세요.",
+            onTextChange = { text.value = it },
+        )
+    }
+}

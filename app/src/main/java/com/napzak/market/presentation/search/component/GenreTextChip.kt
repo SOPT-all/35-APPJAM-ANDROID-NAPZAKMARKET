a1731dc18073e205a3809e27fe8d33ee76f7090d
chrin05 <rinring105@gmail.com>
2025-01-15 17:57:00 +0900

    [UI/#27] 검색화면 내 연관검색어 장르 칩 구현

---
+++ app/src/main/java/com/napzak/market/presentation/search/component/GenreTextChip.kt
@@ -0,0 +1,34 @@
+package com.napzak.market.presentation.search.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.core.designsystem.component.chip.TextChip
+import com.napzak.market.R
+import com.napzak.market.core.designsystem.component.chip.model.CustomChipColors
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+
+@Composable
+fun GenreTextChip() {
+    TextChip(
+        text = stringResource(R.string.search_genre),
+        textStyle = NapzakMarketTheme.typography.capSemi12,
+        modifier = Modifier.clip(RoundedCornerShape(4.dp)),
+        chipColors = CustomChipColors(
+            contentColor = NapzakMarketTheme.colors.gray600,
+            containerColor = NapzakMarketTheme.colors.gray100,
+        ),
+        innerPadding = PaddingValues(horizontal = 8.dp, vertical = 2.dp)
+    )
+}
+
+@Preview
+@Composable
+private fun GenreTextChipPreview() {
+    GenreTextChip()
+}

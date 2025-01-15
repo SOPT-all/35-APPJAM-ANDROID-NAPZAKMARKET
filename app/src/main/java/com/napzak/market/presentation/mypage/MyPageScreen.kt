ed2574ea0afbc090f2995062c148b6f353dc644c
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-15 21:20:12 +0900

    [UI/#25] MyPageScreen

---
+++ app/src/main/java/com/napzak/market/presentation/mypage/MyPageScreen.kt
@@ -0,0 +1,182 @@
+package com.napzak.market.presentation.mypage
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.R
+import com.napzak.market.core.designsystem.component.button.CommonButton
+import com.napzak.market.core.designsystem.component.topbar.CenteredTextTopBar
+import com.napzak.market.presentation.mypage.type.MyPageMenuType
+
+@Composable
+fun MyPageScreen(
+
+    viewModel: MyPageViewModel = viewModel(),
+) {
+
+    val uiState = viewModel.uiState.collectAsState().value
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(NapzakMarketTheme.colors.white),
+    ) {
+        CenteredTextTopBar(
+            title = stringResource(id = R.string.mypage_title),
+            textStyle = NapzakMarketTheme.typography.titleBold20,
+            backgroundColor = NapzakMarketTheme.colors.white,
+            contentColor = NapzakMarketTheme.colors.black,
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(NapzakMarketTheme.colors.gray50),
+        ) {
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(164.dp)
+                    .padding(horizontal = 20.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(containerColor = NapzakMarketTheme.colors.white),
+                border = BorderStroke(1.dp, NapzakMarketTheme.colors.gray200),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(60.dp)
+                                .background(
+                                    color = NapzakMarketTheme.colors.purple10,
+                                    shape = CircleShape,
+                                ),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            AsyncImage(
+                                model = ImageRequest.Builder(LocalContext.current)
+                                    .data(uiState.profileImageUrl)
+                                    .placeholder(R.drawable.ic_profile_basic_60)
+                                    .error(R.drawable.ic_profile_basic_60)
+                                    .build(),
+                                contentDescription = stringResource(id = R.string.profile_image_description),
+                                modifier = Modifier.size(60.dp),
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(14.dp))
+
+                        Text(
+                            text = uiState.nickname,
+                            style = NapzakMarketTheme.typography.titleBold20,
+                            color = NapzakMarketTheme.colors.black,
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+                    CommonButton(
+                        text = stringResource(id = R.string.view_my_market),
+                        onClick = {
+                            //TODO: 내 마켓보기 구현 필요
+                        },
+                        buttonColors = ButtonDefaults.buttonColors(containerColor = NapzakMarketTheme.colors.purple30),
+                        shape = RoundedCornerShape(12.dp),
+                        textStyle = NapzakMarketTheme.typography.bodyBold16,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(44.dp),
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
+            }
+
+            Spacer(modifier = Modifier.height(38.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                MyPageMenuType.entries.forEach { menuType ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .weight(1f)
+                            .clickable {
+                                when (menuType) {
+                                    MyPageMenuType.HISTORY -> {
+                                        // TODO: 판매/구매 내역 화면으로 이동
+                                    }
+
+                                    MyPageMenuType.GENRE -> {
+                                        // TODO: 관심 장르 화면으로 이동
+                                    }
+
+                                    MyPageMenuType.FAVORITE -> {
+                                        // TODO: 찜한 상품 화면으로 이동
+                                    }
+
+                                    MyPageMenuType.RECENT -> {
+                                        // TODO: 최근 본 상품 화면으로 이동
+                                    }
+                                }
+                            }
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(id = menuType.iconResId),
+                            contentDescription = stringResource(id = menuType.titleResId),
+                            modifier = Modifier.size(50.dp),
+                            tint = Color.Unspecified,
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = stringResource(id = menuType.titleResId),
+                            style = NapzakMarketTheme.typography.bodyMedium14,
+                            color = NapzakMarketTheme.colors.gray700,
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(38.dp))
+        }
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyPageScreenPreview() {
+    NapzakMarketTheme {
+        MyPageScreen(
+        )
+    }
+}

877ac82c80bb9c123142ebfd3d20628955e641d6
boiledegg <tjrwns2163@naver.com>
2025-01-16 03:22:57 +0900

    [UI/#29] 배너 구현

---
+++ app/src/main/java/com/napzak/market/core/designsystem/component/indicator/PageIndicator.kt
@@ -0,0 +1,46 @@
+package com.napzak.market.core.designsystem.component.indicator
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+
+@Composable
+fun PageIndicator(
+    imageCount: Int,
+    pagerState: PagerState,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = NapzakMarketTheme.colors.white,
+    selectedColor: Color = NapzakMarketTheme.colors.purple30,
+    unselectedColor: Color = NapzakMarketTheme.colors.gray400,
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(5.dp))
+            .background(color = backgroundColor)
+            .wrapContentSize(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        repeat(imageCount) { iteration ->
+            val color =
+                if (pagerState.currentPage % imageCount == iteration) selectedColor else unselectedColor
+            Box(
+                modifier = Modifier
+                    .size(7.dp)
+                    .clip(CircleShape)
+                    .background(color)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/napzak/market/presentation/home/component/HomeBannerPager.kt
@@ -0,0 +1,104 @@
+package com.napzak.market.presentation.home.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.R
+import com.napzak.market.core.designsystem.component.indicator.PageIndicator
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.delay
+
+@Composable
+fun HomeBannerPager(
+    @DrawableRes bannerImages: List<Int>,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val pagerState = rememberPagerState(
+        initialPage = BANNER_INITIAL_PAGE,
+        pageCount = { Int.MAX_VALUE },
+    )
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(BANNER_DELAY)
+            pagerState.animateScrollToPage(
+                page = pagerState.currentPage + 1,
+                animationSpec = tween(BANNER_TWEEN),
+            )
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HorizontalPager(
+            state = pagerState,
+            modifier = modifier
+        ) { page ->
+            val currentBanner =
+                if (bannerImages.isNotEmpty()) bannerImages[page % bannerImages.size]
+                else 0
+
+            Image(
+                painter = painterResource(id = currentBanner),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize(),
+            )
+
+            /*
+            TODO: 서버 통신 시 사용
+            AsyncImage(
+                model = ImageRequest.Builder(context).data(currentBanner).build(),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize(),
+            )*/
+        }
+
+        PageIndicator(
+            imageCount = bannerImages.size,
+            pagerState = pagerState,
+            modifier = Modifier.padding(top = 10.dp)
+        )
+    }
+}
+
+private const val BANNER_DELAY = 3000L
+private const val BANNER_TWEEN = 1000
+private const val BANNER_INITIAL_PAGE = 0
+
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeBannerPagerPreview() {
+    NapzakMarketTheme {
+        HomeBannerPager(
+            bannerImages = listOf<Int>(
+                R.drawable.img_banner_home1,
+                R.drawable.img_banner_home2,
+            ).toImmutableList(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(360 / 230f),
+        )
+    }
+}

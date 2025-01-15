624bef01298d6fc663d77a497037d6386a9fc91b
boiledegg <tjrwns2163@naver.com>
2025-01-16 03:38:14 +0900

    [UI/#29] 다른 유저 검색 뷰 구현

---
+++ app/src/main/java/com/napzak/market/presentation/home/component/HomeMostSearchedItemGroup.kt
@@ -0,0 +1,116 @@
+package com.napzak.market.presentation.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.napzak.market.R
+import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
+import com.napzak.market.domain.explore.model.ProductItem
+import com.napzak.market.presentation.explore.type.TradeType
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+fun HomeMostSearchedItemGroup(
+    searchedItems: ImmutableList<ProductItem>,
+    onLikeClick: (Int) -> Unit,
+    onItemClick: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val density = LocalDensity.current
+
+    val itemWidth = remember {
+        val metrics = context.resources.displayMetrics
+        val width = metrics.widthPixels
+        with(density) {
+            (width.toFloat() * 150f / 360f).toDp()
+        }
+    }
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.spacedBy(11.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(start = 20.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.home_search_title1),
+                    style = NapzakMarketTheme.typography.titleSemi18.copy(
+                        color = NapzakMarketTheme.colors.gray900
+                    ),
+                )
+
+                Text(
+                    text = stringResource(R.string.home_search_body1),
+                    style = NapzakMarketTheme.typography.bodySemi14.copy(
+                        color = NapzakMarketTheme.colors.gray500
+                    ),
+                    modifier = Modifier.padding(top = 4.dp),
+                )
+            }
+
+            Image(
+                painter = painterResource(R.drawable.img_home_search),
+                contentDescription = null,
+            )
+        }
+        HomeItemRow(
+            modifier = Modifier.padding(top = 16.dp),
+            itemList = searchedItems,
+            itemWidth = itemWidth,
+            onItemClick = onItemClick,
+            onLikeClick = onLikeClick,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeRecommendationGroupPreview() {
+    NapzakMarketTheme {
+        HomeMostSearchedItemGroup(
+            onLikeClick = {},
+            onItemClick = {},
+            searchedItems = mutableListOf<ProductItem>().apply {
+                repeat(4) {
+                    this.add(
+                        ProductItem(
+                            productId = it,
+                            productName = "딸기 마이멜로디 마스코트 인형",
+                            genreName = "산리오",
+                            price = 35000,
+                            uploadTime = "1시간전",
+                            photo = "",
+                            isLiked = false,
+                            tradeType = TradeType.SELL.name,
+                            tradeStatus = "판매중",
+                            isPriceNegotiable = false,
+                        )
+                    )
+                }
+            }.toImmutableList()
+        )
+    }
+}

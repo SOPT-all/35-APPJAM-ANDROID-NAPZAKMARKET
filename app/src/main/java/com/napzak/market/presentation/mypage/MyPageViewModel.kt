19307c65f217332da4a295e0688fa5ba0d4735fe
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-16 01:23:12 +0900

    [MOD/#25] 힐트 어노테이션 적용

--- app/src/main/java/com/napzak/market/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/napzak/market/presentation/mypage/MyPageViewModel.kt
@@ -3,11 +3,14 @@ package com.napzak.market.presentation.mypage
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.napzak.market.presentation.mypage.state.MyPageUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MyPageViewModel : ViewModel() {
+@HiltViewModel
+class MyPageViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow(MyPageUiState())
     val uiState: StateFlow<MyPageUiState> = _uiState
 

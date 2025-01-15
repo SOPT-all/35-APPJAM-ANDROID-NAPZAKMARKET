9c16a6583b2973deb9e75e375f930d0fb1ba0f46
boiledegg <tjrwns2163@naver.com>
2025-01-16 05:06:40 +0900

    [DEL/#29] 미사용 라이브러리 삭제

--- app/src/main/java/com/napzak/market/presentation/home/component/HomeMostSearchedItemGroup.kt
+++ app/src/main/java/com/napzak/market/presentation/home/component/HomeMostSearchedItemGroup.kt
@@ -2,7 +2,6 @@ package com.napzak.market.presentation.home.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth

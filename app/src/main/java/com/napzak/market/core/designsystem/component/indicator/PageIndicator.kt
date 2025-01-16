02c3d976bf4afc900f02f544d845446ec8106f73
boiledegg <tjrwns2163@naver.com>
2025-01-17 01:50:32 +0900

    [DEL/#29] 미사용 라이브러리 제거

--- app/src/main/java/com/napzak/market/core/designsystem/component/indicator/PageIndicator.kt
+++ app/src/main/java/com/napzak/market/core/designsystem/component/indicator/PageIndicator.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip

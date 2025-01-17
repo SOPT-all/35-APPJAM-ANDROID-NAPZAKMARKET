6c597aca2a020d10d4fb574a8877debccb794260
chrin05 <rinring105@gmail.com>
2025-01-18 00:32:04 +0900

    [ADD/#44] 마켓정보 바텀시트 state 추가

---
+++ app/src/main/java/com/napzak/market/presentation/marketinfo/state/MarketInfoBottomSheetState.kt
@@ -0,0 +1,6 @@
+package com.napzak.market.presentation.marketinfo.state
+
+data class MarketInfoBottomSheetState(
+    val isSortBottomSheetVisible: Boolean = false,
+    val isGenreSearchingBottomSheetVisible: Boolean = false,
+)

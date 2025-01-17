6b25248a8a21b39737f39f4a93960bbeceba595a
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-17 23:17:35 +0900

    [ADD/#37] DetailPageUiState

---
+++ app/src/main/java/com/napzak/market/presentation/detailpage/state/DetailPageUiState.kt
@@ -0,0 +1,38 @@
+package com.napzak.market.presentation.detailpage.state
+
+data class DetailPageUiState(
+    val profileImageUrl: String? = null,
+    val productId: Long = 0L,
+    val tradeType: String = "",
+    val genreName: String = "",
+    val productName: String = "",
+    val price: Int = 0,
+    val uploadTime: String = "",
+    val viewCount: Int = 0,
+    val interestCount: Int = 0,
+    val description: String = "",
+    val productCondition: String = "",
+    val standardDeliveryFee: Int = 0,
+    val halfDeliveryFee: Int = 0,
+    val isPriceNegotiable: Boolean = false,
+    val tradeStatus: String = "",
+    val productPhotoUrls: List<String> = emptyList(),
+    val marketInfo: MarketInfoUiState = MarketInfoUiState(),
+    val marketReviews: List<MarketReviewUiState> = emptyList(),
+)
+
+data class MarketInfoUiState(
+    val userId: Long = 0L,
+    val nickname: String = "",
+    val totalProducts: Int = 0,
+    val totalTransactions: Int = 0,
+)
+
+data class MarketReviewUiState(
+    val reviewId: Long = 0L,
+    val reviewerNickname: String = "",
+    val rating: Float = 0.0f,
+    val comment: String = "",
+    val relatedProductId: Long = 0L,
+    val relatedProductName: String = "",
+)

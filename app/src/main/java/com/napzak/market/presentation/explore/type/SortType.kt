21c4ef06305b5ea9edbb21fe5a9530b2cf0daebd
chrin05 <rinring105@gmail.com>
2025-01-17 19:10:17 +0900

    [FIX/#32] 트레일링콤마

--- app/src/main/java/com/napzak/market/presentation/explore/type/SortType.kt
+++ app/src/main/java/com/napzak/market/presentation/explore/type/SortType.kt
@@ -1,10 +1,10 @@
 package com.napzak.market.presentation.explore.type
 
 enum class SortType(
-    val label: String
+    val label: String,
 ) {
     RECENT(label = "최신순"),
     POPULAR(label = "인기순"),
     HIGH_PRICE(label = "고가순"),
-    LOW_PRICE(label = "저가순")
+    LOW_PRICE(label = "저가순"),
 }
--- app/src/main/java/com/napzak/market/presentation/explore/type/TradeType.kt
+++ app/src/main/java/com/napzak/market/presentation/explore/type/TradeType.kt
@@ -1,7 +1,7 @@
 package com.napzak.market.presentation.explore.type
 
 enum class TradeType(
-    val label: String
+    val label: String,
 ) {
     SELL(label = "팔아요"),
     BUY(label = "구해요"),

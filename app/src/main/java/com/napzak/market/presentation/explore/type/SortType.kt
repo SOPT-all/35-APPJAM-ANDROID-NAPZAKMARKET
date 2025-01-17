9c625f6881568a1e5444d4371c03fd4809d6c625
chrin05 <rinring105@gmail.com>
2025-01-17 18:40:01 +0900

    [FIX/#32] 불필요한 함수 제거

--- app/src/main/java/com/napzak/market/presentation/explore/type/SortType.kt
+++ app/src/main/java/com/napzak/market/presentation/explore/type/SortType.kt
@@ -6,11 +6,5 @@ enum class SortType(
     RECENT(label = "최신순"),
     POPULAR(label = "인기순"),
     HIGH_PRICE(label = "고가순"),
-    LOW_PRICE(label = "저가순");
-
-    companion object {
-        fun getSortLabel(type: String): String {
-            return entries.find { it.name == type }?.label ?: "잘못된 SortType 값 입니다."
-        }
-    }
+    LOW_PRICE(label = "저가순")
 }

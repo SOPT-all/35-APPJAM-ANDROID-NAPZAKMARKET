00a6f273e04211f0ea1393344bd96fce680d8c5b
chrin05 <rinring105@gmail.com>
2025-01-18 02:24:28 +0900

    [FEAT/#44] navigation 구현

---
+++ app/src/main/java/com/napzak/market/presentation/marketinfo/navigation/MarketInfoNavigation.kt
@@ -0,0 +1,36 @@
+package com.napzak.market.presentation.marketinfo.navigation
+
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.napzak.market.core.common.navigation.Route
+import com.napzak.market.presentation.marketinfo.MarketInfoRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToMarketInfo(
+    storeId: Long,
+    navOptions: NavOptions? = null,
+) = navigate(MarketInfo(storeId), navOptions)
+
+fun NavGraphBuilder.marketInfoGraph(
+    modifier: Modifier = Modifier,
+) {
+    composable<MarketInfo> { backStackEntry ->
+        val marketInfo: MarketInfo = backStackEntry.toRoute()
+
+        MarketInfoRoute(
+            storeId = marketInfo.storeId,
+            onBackButtonClick = { },
+            onProductDetailNavigate = { },
+            modifier = modifier,
+        )
+    }
+}
+
+@Serializable
+data class MarketInfo(
+    val storeId: Long,
+) : Route

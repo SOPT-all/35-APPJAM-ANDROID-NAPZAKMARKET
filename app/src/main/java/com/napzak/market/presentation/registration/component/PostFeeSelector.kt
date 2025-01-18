a2bf5452109e2e75b5c1b4afc4c889b3f53213bd
Jong <jm991014@gmail.com>
2025-01-18 09:34:13 +0900

    [CHORE/#39] string명 변경

--- app/src/main/java/com/napzak/market/presentation/registration/component/PostFeeSelector.kt
+++ app/src/main/java/com/napzak/market/presentation/registration/component/PostFeeSelector.kt
@@ -17,8 +17,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.napzak.market.R.string.regi_post_price_include
-import com.napzak.market.R.string.regi_post_price_exclude
+import com.napzak.market.R.string.regi_post_fee_include
+import com.napzak.market.R.string.regi_post_fee_exclude
 import com.napzak.market.core.common.extension.noRippleClickable
 import com.napzak.market.core.designsystem.theme.NapzakMarketTheme
 
@@ -39,8 +39,8 @@ fun PostFeeSelector(
     modifier: Modifier = Modifier,
 ) {
     val feeTab = listOf(
-        stringResource(regi_post_price_include),
-        stringResource(regi_post_price_exclude),
+        stringResource(regi_post_fee_include),
+        stringResource(regi_post_fee_exclude),
     )
     Box(
         modifier = modifier

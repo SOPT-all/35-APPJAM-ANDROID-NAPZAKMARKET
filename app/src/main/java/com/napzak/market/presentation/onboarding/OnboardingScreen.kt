52471a6d4ed900de4a1ed5fc3e3c1e3d66819944
boiledegg <tjrwns2163@naver.com>
2025-01-19 22:05:35 +0900

    [CHORE/#47] 온보딩 그라디엔트 수정

--- app/src/main/java/com/napzak/market/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/napzak/market/presentation/onboarding/OnboardingScreen.kt
@@ -1,6 +1,9 @@
 package com.napzak.market.presentation.onboarding
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
@@ -15,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
@@ -24,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.napzak.market.R
@@ -94,7 +99,8 @@ private fun OnboardingScreen(
             onSearchButtonClick = onSearchButtonClick,
             modifier = Modifier
                 .padding(horizontal = 20.dp)
-                .padding(top = 32.dp),
+                .padding(top = 32.dp)
+                .zIndex(1f),
         )
 
         GenreChipButtonGroup(
@@ -103,10 +109,14 @@ private fun OnboardingScreen(
             onResetClick = onResetClick,
             contentPaddingValues = PaddingValues(end = 20.dp),
             modifier = Modifier
-                .padding(start = 20.dp, top = 16.dp),
+                .padding(start = 20.dp, top = 16.dp)
+                .zIndex(1f),
         )
 
-        Box(modifier = Modifier.weight(1f)) {
+        Box(modifier = Modifier
+            .padding(top = 30.dp)
+            .weight(1f)
+            .zIndex(-1f)) {
             when (uiState.genreList) {
                 is UiState.Loading -> {}
                 is UiState.Empty -> {}
@@ -139,13 +149,15 @@ private fun BoxScope.SuccessScreen(
     onGenreClick: (Genre) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val lazyGridState = rememberLazyGridState()
+
     CompositionLocalProvider(
         value = LocalOverscrollConfiguration provides null,
         content = {
             LazyVerticalGrid(
                 columns = GridCells.Fixed(3),
+                state = lazyGridState,
                 contentPadding = PaddingValues(
-                    top = 30.dp,
                     bottom = 54.dp,
                 ),
                 verticalArrangement = Arrangement.spacedBy(20.dp),
@@ -162,19 +174,26 @@ private fun BoxScope.SuccessScreen(
                 }
             }
 
-            GradientBox(
-                modifier = Modifier.align(Alignment.TopCenter),
-                brushColors = listOf(
-                    NapzakMarketTheme.colors.white,
-                    Color.Transparent,
-                ),
-            )
+            AnimatedVisibility(
+                visible = lazyGridState.canScrollBackward,
+                enter = slideInVertically { fullHeight -> -fullHeight },
+                exit = slideOutVertically { fullHeight -> -fullHeight },
+            ) {
+                GradientBox(
+                    modifier = Modifier.align(Alignment.TopCenter),
+                    brushColors = listOf(
+                        NapzakMarketTheme.colors.white,
+                        Color.Transparent,
+                    ),
+                )
+            }
 
             GradientBox(
                 modifier = Modifier.align(Alignment.BottomCenter),
                 brushColors = listOf(
                     Color.Transparent,
                     NapzakMarketTheme.colors.white,
+
                 ),
             )
         }

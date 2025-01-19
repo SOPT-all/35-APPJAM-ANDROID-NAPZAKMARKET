b07ae5c5cf59069dce8db851ff66f1d9842eda2f
Jong <jm991014@gmail.com>
2025-01-20 02:42:33 +0900

    [ADD/#39] UiState 상태값 추가

--- app/src/main/java/com/napzak/market/presentation/registration/state/RegistrationUiState.kt
+++ app/src/main/java/com/napzak/market/presentation/registration/state/RegistrationUiState.kt
@@ -5,12 +5,16 @@ import androidx.compose.runtime.Immutable
 @Immutable
 data class RegistrationUiState(
     val imageUrlList: List<String> = emptyList(),
+    val searchTerm: String = "",
     val genre: String = "",
+    val genreList: List<String> = emptyList(),
     val title: String = "",
     val description: String = "",
+    val isButtonEnabled: Boolean = false,
 
     val productCondition: Int = -1,
     val productSalePrice: String = "",
+    val isPostFeeIncluded: Boolean = false,
     val normalPostFee: String = "",
     val halfPostFee: String = "",
 

fa1bfd9e2fedece256f4e26f99332e3233ed7930
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-19 23:54:58 +0900

    [MOD/#37] ProductConditionType 아주 좋은 상태

--- app/src/main/java/com/napzak/market/presentation/detailpage/type/ProductConditionType.kt
+++ app/src/main/java/com/napzak/market/presentation/detailpage/type/ProductConditionType.kt
@@ -1,18 +1,18 @@
 package com.napzak.market.presentation.detailpage.type
 
 enum class ProductCondition(val label: String) {
+    EXCELLENT_CONDITION("아주 좋은 상태"),
     BRAND_NEW("미개봉"),
     SLIGHTLY_USED("약간의 사용감"),
-    USED("사용감 있음"),
-    LIGHTLY_USED("약간 사용감 있음");
+    USED("사용감 있음");
 
     companion object {
         fun fromCondition(condition: String?): ProductCondition {
             return when (condition) {
+                EXCELLENT_CONDITION.label -> EXCELLENT_CONDITION
                 BRAND_NEW.label -> BRAND_NEW
                 SLIGHTLY_USED.label -> SLIGHTLY_USED
                 USED.label -> USED
-                LIGHTLY_USED.label -> LIGHTLY_USED
                 else -> BRAND_NEW
             }
         }

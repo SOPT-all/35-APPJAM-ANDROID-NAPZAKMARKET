2476cda8d31f52fbf5a10d5cc2140606fe587d70
boiledegg <tjrwns2163@naver.com>
2025-01-21 08:16:09 +0900

    [FEAT/#63] AuthInterceptor 구현
    - 헤더에 토큰 자동 추가

---
+++ app/src/main/java/com/napzak/market/core/network/AuthInterceptor.kt
@@ -0,0 +1,37 @@
+package com.napzak.market.core.network
+
+import com.napzak.market.core.local.TokenDataStore
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val tokenDataStore: TokenDataStore
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        val authRequest = runBlocking {
+            val token = tokenDataStore.getAccessToken()
+            if (token?.isNotBlank() == true) {
+                originalRequest.newBuilder().newAuthBuilder().build()
+            } else {
+                originalRequest
+            }
+        }
+
+        val response = chain.proceed(authRequest)
+
+        return response
+    }
+
+    private suspend fun Request.Builder.newAuthBuilder() =
+        this.addHeader(AUTHORIZATION, "$BEARER ${tokenDataStore.getAccessToken()}")
+
+    companion object {
+        private const val BEARER = "Bearer"
+        private const val AUTHORIZATION = "Authorization"
+    }
+}

a1bb352e7c411c5106e3662a131666f876f5f93e
boiledegg <tjrwns2163@naver.com>
2025-01-21 20:22:24 +0900

    [DEL/#69] 배너 더미 삭제

--- app/src/main/java/com/napzak/market/presentation/home/state/HomeUiState.kt
+++ app/src/main/java/com/napzak/market/presentation/home/state/HomeUiState.kt
@@ -1,6 +1,5 @@
 package com.napzak.market.presentation.home.state
 
-import com.napzak.market.R
 import com.napzak.market.core.common.state.UiState
 import com.napzak.market.core.type.TradeType
 import com.napzak.market.domain.explore.model.ProductItem
@@ -13,13 +12,6 @@ data class HomeUiState(
     val searchItems: UiState<List<ProductItem>> = UiState.Loading,
 ) {
     companion object {
-        val dummyBanner = UiState.Success(
-            listOf(
-                R.drawable.img_banner_home1,
-                R.drawable.img_banner_home2,
-            )
-        )
-
         val dummyData = UiState.Success(
             listOf(
                 ProductItem(

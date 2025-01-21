436f2d8eb698247122b7e80b6caa57d8beb359c7
chrin05 <rinring105@gmail.com>
2025-01-22 06:45:15 +0900

    [FIX/#79] 속성 추가

--- app/src/main/java/com/napzak/market/domain/marketinfo/model/ProductListFilter.kt
+++ app/src/main/java/com/napzak/market/domain/marketinfo/model/ProductListFilter.kt
@@ -1,6 +1,7 @@
 package com.napzak.market.domain.marketinfo.model
 
 data class ProductListFilter(
+    val storeId: Long,
     val sortOption: String,
     val genreId: List<Long>?,
     val isOnSale: Boolean,

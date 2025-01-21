1d327100247abfb146e7a6d96761c321bc199c7a
boiledegg <tjrwns2163@naver.com>
2025-01-21 22:08:21 +0900

    [UI/#75] 팔아요/구해요 매핑 UI 컴포넌트 수정

--- app/src/main/java/com/napzak/market/presentation/home/component/HomeItemMapper.kt
+++ app/src/main/java/com/napzak/market/presentation/home/component/HomeItemMapper.kt
@@ -3,6 +3,7 @@ package com.napzak.market.presentation.home.component
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.napzak.market.core.designsystem.component.item.NapzakBuyItem
+import com.napzak.market.core.designsystem.component.item.NapzakSellItem
 import com.napzak.market.domain.home.model.ProductItem
 
 @Composable
@@ -13,18 +14,33 @@ fun HomeItemMapper(
     modifier: Modifier = Modifier,
 ) {
     with(item) {
-        NapzakBuyItem(
-            title = productName,
-            genre = genreName,
-            price = price.toString(),
-            imgUrl = photo,
-            isLiked = isLiked,
-            isMyItem = false,
-            isOfferPossible = isPriceNegotiable,
-            createdTime = uploadTime,
-            onItemClick = onItemClick,
-            onLikeClick = onLikeClick,
-            modifier = modifier,
-        )
+        if (item.tradeType == "BUY") {
+            NapzakBuyItem(
+                title = productName,
+                genre = genreName,
+                price = price.toString(),
+                imgUrl = photo,
+                isLiked = isLiked,
+                isMyItem = false,
+                isOfferPossible = isPriceNegotiable,
+                createdTime = uploadTime,
+                onItemClick = onItemClick,
+                onLikeClick = onLikeClick,
+                modifier = modifier,
+            )
+        } else {
+            NapzakSellItem(
+                title = productName,
+                genre = genreName,
+                price = price.toString(),
+                imgUrl = photo,
+                isLiked = isLiked,
+                isMyItem = false,
+                createdTime = uploadTime,
+                onItemClick = onItemClick,
+                onLikeClick = onLikeClick,
+                modifier = modifier,
+            )
+        }
     }
 }

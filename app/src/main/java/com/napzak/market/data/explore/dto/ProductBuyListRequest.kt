632524b0cd701349d584e6e96a3a8f7918b65514
chrin05 <rinring105@gmail.com>
2025-01-21 22:00:56 +0900

    [ADD/#68] 상품목록조회 요청 dto 추가

---
+++ app/src/main/java/com/napzak/market/data/explore/dto/ProductBuyListRequest.kt
@@ -0,0 +1,14 @@
+package com.napzak.market.data.explore.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProductBuyListRequest(
+    @SerialName("sortOption")
+    val sortOption: String,
+    @SerialName("genreId")
+    val genreIds: List<Long>?,
+    @SerialName("isOnSale")
+    val isOnSale: Boolean,
+)

35ca307340b664691501bc2144dca3a58945ea2a
boiledegg <tjrwns2163@naver.com>
2025-01-21 20:21:34 +0900

    [UI/#69] UI에 배너 연결

--- app/src/main/java/com/napzak/market/domain/home/model/HomeBanner.kt
+++ app/src/main/java/com/napzak/market/domain/home/model/HomeBanner.kt
@@ -1,8 +1,8 @@
 package com.napzak.market.domain.home.model
 
 data class HomeBanner(
-    val bannerId: Long,
-    val bannerPhoto: String,
-    val bannerUrl: String,
-    val bannerSequence: Int,
+    val bannerId: Long = 0,
+    val bannerPhoto: String = "",
+    val bannerUrl: String = "",
+    val bannerSequence: Int = 0,
 )
--- app/src/main/java/com/napzak/market/presentation/home/state/HomeUiState.kt
+++ app/src/main/java/com/napzak/market/presentation/home/state/HomeUiState.kt
@@ -4,9 +4,10 @@ import com.napzak.market.R
 import com.napzak.market.core.common.state.UiState
 import com.napzak.market.core.type.TradeType
 import com.napzak.market.domain.explore.model.ProductItem
+import com.napzak.market.domain.home.model.HomeBanner
 
 data class HomeUiState(
-    val bannerImages: UiState<List<Int>> = UiState.Loading,
+    val bannerImages: UiState<List<HomeBanner>> = UiState.Loading,
     val recommendedItems: UiState<List<ProductItem>> = UiState.Loading,
     val popularItems: UiState<List<ProductItem>> = UiState.Loading,
     val searchItems: UiState<List<ProductItem>> = UiState.Loading,

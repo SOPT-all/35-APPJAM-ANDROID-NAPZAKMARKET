6614b1f7c5f927ea9f869a8102182d15591a4c8f
chrin05 <rinring105@gmail.com>
2025-01-21 21:25:23 +0900

    [RENAME/#68] 이름 변경

--- app/src/main/java/com/napzak/market/data/explore/dto/ProductBuyListResponse.kt
+++ app/src/main/java/com/napzak/market/data/explore/dto/ProductBuyListResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GetProductBuyListResponse(
+data class ProductBuyListResponse(
     @SerialName("ProductBuyList")
     val productBuyList: List<ProductBuyItem>,
 ) {
--- app/src/main/java/com/napzak/market/data/explore/dto/ProductSellListResponse.kt
+++ app/src/main/java/com/napzak/market/data/explore/dto/ProductSellListResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GetProductSellListResponse(
+data class ProductSellListResponse(
     @SerialName("productSellList")
     val productSellList: List<ProductSellItem>,
 ) {

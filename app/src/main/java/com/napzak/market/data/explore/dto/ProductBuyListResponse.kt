6d7446abb230a40c4e3acdff698716a6cd182aaa
chrin05 <rinring105@gmail.com>
2025-01-21 23:18:13 +0900

    [FIX/#68] 이름 수정

--- app/src/main/java/com/napzak/market/data/explore/dto/ProductBuyListResponse.kt
+++ app/src/main/java/com/napzak/market/data/explore/dto/ProductBuyListResponse.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ProductBuyListResponse(
-    @SerialName("ProductBuyList")
+    @SerialName("productBuyList")
     val productBuyList: List<ProductBuyItem>,
 ) {
     @Serializable

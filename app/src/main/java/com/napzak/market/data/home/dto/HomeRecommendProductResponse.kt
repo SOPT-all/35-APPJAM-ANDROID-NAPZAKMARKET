e547070e100fb84bfd487aa04a4a3d4cb1eb8640
boiledegg <tjrwns2163@naver.com>
2025-01-21 22:04:11 +0900

    [FEAT/#75] 추천상품 data 추가

---
+++ app/src/main/java/com/napzak/market/data/home/dto/HomeRecommendProductResponse.kt
@@ -0,0 +1,62 @@
+package com.napzak.market.data.home.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class HomeRecommendProductResponse(
+    @SerialName("productBuyList")
+    val productBuyList: List<ProductBuyItem>,
+    @SerialName("productSellList")
+    val productSellList: List<ProductSellItem>
+)
+
+@Serializable
+data class ProductBuyItem(
+    @SerialName("productId")
+    val productId: Long,
+    @SerialName("genreName")
+    val genreName: String,
+    @SerialName("productName")
+    val productName: String,
+    @SerialName("photo")
+    val photo: String,
+    @SerialName("price")
+    val price: Int,
+    @SerialName("uploadTime")
+    val uploadTime: String,
+    @SerialName("isInterested")
+    val isInterested: Boolean,
+    @SerialName("tradeType")
+    val tradeType: String,
+    @SerialName("tradeStatus")
+    val tradeStatus: String,
+    @SerialName("isPriceNegotiable")
+    val isPriceNegotiable: Boolean,
+    @SerialName("isOwnedByCurrentUser")
+    val isOwnedByCurrentUser: Boolean
+)
+
+@Serializable
+data class ProductSellItem(
+    @SerialName("productId")
+    val productId: Long,
+    @SerialName("genreName")
+    val genreName: String,
+    @SerialName("productName")
+    val productName: String,
+    @SerialName("photo")
+    val photo: String,
+    @SerialName("price")
+    val price: Int,
+    @SerialName("uploadTime")
+    val uploadTime: String,
+    @SerialName("isInterested")
+    val isInterested: Boolean,
+    @SerialName("tradeType")
+    val tradeType: String,
+    @SerialName("tradeStatus")
+    val tradeStatus: String,
+    @SerialName("isOwnedByCurrentUser")
+    val isOwnedByCurrentUser: Boolean
+)
--- app/src/main/java/com/napzak/market/data/home/service/HomeService.kt
+++ app/src/main/java/com/napzak/market/data/home/service/HomeService.kt
@@ -2,10 +2,13 @@ package com.napzak.market.data.home.service
 
 import com.napzak.market.core.network.BaseResponse
 import com.napzak.market.data.home.dto.HomeBannerResponse
+import com.napzak.market.data.home.dto.HomeRecommendProductResponse
 import retrofit2.http.GET
 
 interface HomeService {
     @GET("banners/home")
     suspend fun getHomeBanner(): BaseResponse<HomeBannerResponse>
 
+    @GET("products/home/recommend")
+    suspend fun getHomeRecommendProduct(): BaseResponse<HomeRecommendProductResponse>
 }

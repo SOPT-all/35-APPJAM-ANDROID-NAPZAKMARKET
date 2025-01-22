aaefd32a87e871062f8e2f8f10ac50472a65eb3c
chrin05 <rinring105@gmail.com>
2025-01-23 05:23:50 +0900

    [FIX/#103] MyPage DTO 수정

--- app/src/main/java/com/napzak/market/data/mypage/dto/MyPageInfoResponse.kt
+++ app/src/main/java/com/napzak/market/data/mypage/dto/MyPageInfoResponse.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 data class MyPageInfoResponse(
     @SerialName("storeId")
     val storeId: Long,
-    @SerialName("storeNickname")
+    @SerialName("storeNickName")
     val storeNickname: String,
     @SerialName("storePhoto")
     val storePhoto: String,

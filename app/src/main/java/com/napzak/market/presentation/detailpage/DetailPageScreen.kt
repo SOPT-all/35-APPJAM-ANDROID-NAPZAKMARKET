17257d20aeb9d13772aa8697e2279a6b4dbf496a
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-23 05:56:48 +0900

    [MOD/#95] 명명 변경 title -> nickname

--- app/src/main/java/com/napzak/market/presentation/detailpage/DetailPageScreen.kt
+++ app/src/main/java/com/napzak/market/presentation/detailpage/DetailPageScreen.kt
@@ -107,7 +107,7 @@ fun DetailPageScreen(
     Scaffold(
         topBar = {
             BackTopBar(
-                title = "",
+                nickname = "",
                 onBackClick = onBackClick,
             )
         },

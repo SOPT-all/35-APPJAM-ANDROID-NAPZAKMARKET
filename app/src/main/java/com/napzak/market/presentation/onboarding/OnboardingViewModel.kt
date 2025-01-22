23a9b25305d9b13626ef546af79a7aaadf1121e0
boiledegg <tjrwns2163@naver.com>
2025-01-22 16:23:21 +0900

    [MOD/#86] 뷰모델 수정

--- app/src/main/java/com/napzak/market/presentation/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/napzak/market/presentation/onboarding/OnboardingViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.napzak.market.core.common.state.UiState
 import com.napzak.market.domain.genre.model.Genre
+import com.napzak.market.domain.genre.usecase.ImageGenreSearchUseCase
 import com.napzak.market.presentation.onboarding.state.OnboardingUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.FlowPreview
@@ -13,11 +14,12 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class OnboardingViewModel @Inject constructor(
-
+    private val genreSearchUseCase: ImageGenreSearchUseCase,
 ) : ViewModel() {
     private val _searchTerm: MutableStateFlow<String> = MutableStateFlow("")
     val searchTerm = _searchTerm.asStateFlow()
@@ -25,6 +27,10 @@ class OnboardingViewModel @Inject constructor(
     private val _uiState: MutableStateFlow<OnboardingUiState> =
         MutableStateFlow(OnboardingUiState())
     val uiState = _uiState.asStateFlow()
+
+    init {
+        getGenreList("")
+    }
 
     fun changeSearchText(newValue: String) = viewModelScope.launch {
         updateSearchValue(newValue)
@@ -40,7 +46,29 @@ class OnboardingViewModel @Inject constructor(
         }
 
     // TODO: 서버 통신으로 대체
-    private fun getGenreList(searchTerm: String) {
+    private fun getGenreList(searchTerm: String) = viewModelScope.launch {
+        Timber.tag("getGenreList").d("searchTerm: $searchTerm")
+        genreSearchUseCase.invoke(searchTerm)
+            .onSuccess { response ->
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        genreList = UiState.Success(
+                            response
+                        )
+                    )
+                }
+            }
+            .onFailure {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        genreList = UiState.Failure(
+                            it.message.toString()
+                        )
+                    )
+                }
+            }
+    }
+    /*{
         _uiState.update { currentState ->
             currentState.copy(
                 genreList = UiState.Success(
@@ -54,7 +82,7 @@ class OnboardingViewModel @Inject constructor(
                 ),
             )
         }
-    }
+    }*/
 
     fun selectGenre(genre: Genre) {
         if (_uiState.value.selectedGenreList.map { it.genreId }.contains(genre.genreId)) {

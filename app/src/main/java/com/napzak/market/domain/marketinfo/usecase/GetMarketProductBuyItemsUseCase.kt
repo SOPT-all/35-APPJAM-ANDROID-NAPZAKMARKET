1e7f3e7efa505850e427696594a9e89905118b39
chrin05 <rinring105@gmail.com>
2025-01-22 19:34:10 +0900

    [FEAT/#79] usecase 구현

---
+++ app/src/main/java/com/napzak/market/domain/marketinfo/usecase/GetMarketProductBuyItemsUseCase.kt
@@ -0,0 +1,34 @@
+package com.napzak.market.domain.marketinfo.usecase
+
+import com.napzak.market.domain.genre.model.Genre
+import com.napzak.market.domain.marketinfo.model.ProductItem
+import com.napzak.market.domain.marketinfo.model.ProductListFilter
+import com.napzak.market.domain.marketinfo.repository.MarketInfoRepository
+import javax.inject.Inject
+
+class GetMarketProductBuyItemsUseCase @Inject constructor(
+    private val marketInfoRepository: MarketInfoRepository,
+) {
+    suspend operator fun invoke(
+        storeId: Long,
+        sortType: String,
+        genreItems: List<Genre>?,
+        isOnSale: Boolean,
+    ): Result<List<ProductItem>> {
+        val genreIds = if (genreItems?.isNotEmpty() == true) {
+            genreItems.map { it.genreId }
+        } else {
+            null
+        }
+
+        return marketInfoRepository.fetchMarketProductBuyItems(
+            ProductListFilter(
+                storeId = storeId,
+                sortOption = sortType,
+                genreId = genreIds,
+                isOnSale = isOnSale,
+                isUnopened = false,
+            )
+        )
+    }
+}

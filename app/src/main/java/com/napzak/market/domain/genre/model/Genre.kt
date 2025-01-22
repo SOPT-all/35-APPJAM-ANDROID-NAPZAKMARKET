dba23e4930ab55f914949c3d771f439cba621ad4
chrin05 <rinring105@gmail.com>
2025-01-22 23:01:00 +0900

    [FIX/#78] 주석 삭제

--- app/src/main/java/com/napzak/market/domain/genre/model/Genre.kt
+++ app/src/main/java/com/napzak/market/domain/genre/model/Genre.kt
@@ -7,5 +7,5 @@ import kotlinx.parcelize.Parcelize
 data class Genre (
     val genreId: Long,
     val genreName: String,
-    val genreImgUrl: String? = null, // TODO: 추후 삭제 예정
+    val genreImgUrl: String? = null,
 ) : Parcelable

970269fafdbc1d65dc954febe6ae43da5e0f9b07
chrin05 <rinring105@gmail.com>
2025-01-23 05:07:17 +0900

    [CHORE/#103] (Explore) code 정리

--- app/src/main/java/com/napzak/market/presentation/explore/explore/ExploreScreen.kt
+++ app/src/main/java/com/napzak/market/presentation/explore/explore/ExploreScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.napzak.market.core.common.state.UiState
 import com.napzak.market.core.designsystem.component.textField.SearchBox
 import com.napzak.market.domain.explore.model.ProductItem
@@ -47,6 +48,7 @@ import com.napzak.market.presentation.explore.explore.component.TradeTypeTab
 import com.napzak.market.presentation.explore.explore.state.ExploreBottomSheetState
 import com.napzak.market.presentation.explore.explore.state.ExploreUiState
 import com.napzak.market.presentation.explore.explore.type.ExploreScreenType
+import kotlinx.coroutines.coroutineScope
 
 @Composable
 fun ExploreRoute(
@@ -106,13 +108,13 @@ fun ExploreRoute(
         onSortButtonClick = { viewModel.updateBottomSheetVisibility(BottomSheetType.SORT) },
         onItemClick = onProductDetailNavigate,
         onLikeClick = viewModel::setProductInterest,
-        onDismissRequest = { viewModel.updateBottomSheetVisibility(it) },
+        onDismissRequest = viewModel::updateBottomSheetVisibility,
         onSortItemClick = {
             viewModel.updateSortType(it)
             viewModel.updateBottomSheetVisibility(BottomSheetType.SORT)
             viewModel.initScrollState(coroutineScope, gridState)
         },
-        onTextChange = { viewModel.changeSearchText(it) },
+        onTextChange = viewModel::changeSearchText,
         onGenreSelectButtonClick = viewModel::updateSelectedGenreList,
     )
 }

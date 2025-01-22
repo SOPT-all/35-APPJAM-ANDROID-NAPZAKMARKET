e728de503819a36a44a03977c543e7e4b19e61bd
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-23 05:57:53 +0900

    [MOD/#95] di

---
+++ app/src/main/java/com/napzak/market/data/chat/di/ChatRepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.napzak.market.data.chat.di
+
+import com.napzak.market.data.chat.repositoryimpl.ChatRepositoryImpl
+import com.napzak.market.domain.chat.repository.ChatRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class ChatRepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindChatRepository(
+        impl: ChatRepositoryImpl,
+    ): ChatRepository
+}

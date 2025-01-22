d0e9ed21646f8864203fc786dbfc2e142f472da1
chrin05 <rinring105@gmail.com>
2025-01-22 23:59:28 +0900

    [FEAT/#78] (Search) 검색어(장르) 연관검색어 API

--- app/src/main/java/com/napzak/market/presentation/explore/search/SearchViewModel.kt
+++ app/src/main/java/com/napzak/market/presentation/explore/search/SearchViewModel.kt
@@ -3,7 +3,7 @@ package com.napzak.market.presentation.explore.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.napzak.market.core.common.state.UiState
-import com.napzak.market.domain.genre.model.Genre
+import com.napzak.market.domain.genre.respository.GenreRepository
 import com.napzak.market.presentation.explore.search.state.RelatedGenreList
 import com.napzak.market.presentation.explore.search.state.SearchUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,93 +18,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-
+    private val genreRepository: GenreRepository,
 ) : ViewModel() {
     private val _searchTerm: MutableStateFlow<String> = MutableStateFlow("")
     val searchTerm = _searchTerm.asStateFlow()
 
     private val _uiState = MutableStateFlow(SearchUiState())
     val uiState = _uiState.asStateFlow()
-
-    fun getGenreList(searchTerm: String) {
-        /* TODO: 연관검색어 조회 API 연결 */
-        val genreList = if (searchTerm.isEmpty()) {
-            emptyList<Genre>()
-        } else {
-            listOf(
-                Genre(
-                    genreId = 1,
-                    genreName = "나루토",
-                ),
-                Genre(
-                    genreId = 2,
-                    genreName = "원피스",
-                ),
-                Genre(
-                    genreId = 3,
-                    genreName = "블리치",
-                ),
-                Genre(
-                    genreId = 4,
-                    genreName = "귀멸의 칼날",
-                ),
-                Genre(
-                    genreId = 5,
-                    genreName = "주술회전",
-                ),
-                Genre(
-                    genreId = 6,
-                    genreName = "진격의 거인",
-                ),
-                Genre(
-                    genreId = 7,
-                    genreName = "데스노트",
-                ),
-                Genre(
-                    genreId = 8,
-                    genreName = "짱구는 못말려",
-                ),
-                Genre(
-                    genreId = 9,
-                    genreName = "도라에몽",
-                ),
-                Genre(
-                    genreId = 10,
-                    genreName = "강철의 연금술사",
-                ),
-                Genre(
-                    genreId = 11,
-                    genreName = "체인소맨",
-                ),
-                Genre(
-                    genreId = 12,
-                    genreName = "원펀맨",
-                ),
-                Genre(
-                    genreId = 13,
-                    genreName = "드래곤볼",
-                ),
-                Genre(
-                    genreId = 14,
-                    genreName = "명탐정 코난",
-                ),
-                Genre(
-                    genreId = 15,
-                    genreName = "슬램덩크",
-                )
-            )
-        }
-
-        updateLoadState(
-            loadState = UiState.Success(
-                RelatedGenreList(
-                    genreList = genreList.filter {
-                        it.genreName.contains(searchTerm)
-                    }
-                )
-            )
-        )
-    }
 
     fun changeSearchText(newValue: String) = viewModelScope.launch {
         updateSearchValue(newValue)
@@ -114,10 +34,38 @@ class SearchViewModel @Inject constructor(
     fun updateSearchValue(newValue: String) = _searchTerm.update { newValue }
 
     @OptIn(FlowPreview::class)
-    private suspend fun debounceSearch() = _searchTerm.debounce(DEBOUNCE_DELAY)
-        .collectLatest { debounced ->
-            getGenreList(debounced)
+    fun debounceSearch() = viewModelScope.launch {
+        _searchTerm.debounce(DEBOUNCE_DELAY)
+            .collectLatest { debounced ->
+                getGenreList(debounced)
+            }
+    }
+
+    fun getGenreList(searchTerm: String) = viewModelScope.launch {
+        if (searchTerm.isBlank()) {
+            updateLoadState(
+                UiState.Success(
+                    RelatedGenreList(genreList = emptyList())
+                )
+            )
+        } else {
+            genreRepository.fetchSearchResultGenreItems(searchTerm)
+                .onSuccess { response ->
+                    updateLoadState(
+                        UiState.Success(
+                            RelatedGenreList(genreList = response)
+                        )
+                    )
+                }
+                .onFailure {
+                    updateLoadState(
+                        UiState.Failure(
+                            it.message.toString()
+                        )
+                    )
+                }
         }
+    }
 
 
     private fun updateLoadState(loadState: UiState<RelatedGenreList>) =

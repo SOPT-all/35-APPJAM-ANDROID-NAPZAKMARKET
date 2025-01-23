3115a89af0cf5f23b96c4c1687fae00e37d19f28
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-23 14:11:34 +0900

    [MOD/#95] 개행

--- app/src/main/java/com/napzak/market/data/chat/dto/ChatInfoResponse.kt
+++ app/src/main/java/com/napzak/market/data/chat/dto/ChatInfoResponse.kt
@@ -5,10 +5,16 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ChatInfoResponse(
-    @SerialName("nickname") val nickname: String,
-    @SerialName("firstPhoto") val firstPhoto: String,
-    @SerialName("tradeType") val tradeType: String,
-    @SerialName("title") val title: String,
-    @SerialName("price") val price: Int,
-    @SerialName("isPriceNegotiable") val isPriceNegotiable: Boolean,
+    @SerialName("nickname")
+    val nickname: String,
+    @SerialName("firstPhoto")
+    val firstPhoto: String,
+    @SerialName("tradeType")
+    val tradeType: String,
+    @SerialName("title")
+    val title: String,
+    @SerialName("price")
+    val price: Int,
+    @SerialName("isPriceNegotiable")
+    val isPriceNegotiable: Boolean,
 )

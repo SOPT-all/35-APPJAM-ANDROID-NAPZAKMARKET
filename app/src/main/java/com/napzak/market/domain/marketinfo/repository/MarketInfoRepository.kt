fba51f8b25ef4fe17348421cacad741cc7f377d9
chrin05 <rinring105@gmail.com>
2025-01-23 23:16:31 +0900

    [CHORE/#111] 불필요한 import 삭제

--- app/src/main/java/com/napzak/market/domain/marketinfo/repository/MarketInfoRepository.kt
+++ app/src/main/java/com/napzak/market/domain/marketinfo/repository/MarketInfoRepository.kt
@@ -1,8 +1,6 @@
 package com.napzak.market.domain.marketinfo.repository
 
 import com.napzak.market.domain.marketinfo.model.MarketInfo
-import com.napzak.market.domain.marketinfo.model.ProductItem
-import com.napzak.market.domain.marketinfo.model.ProductListFilter
 
 interface MarketInfoRepository {
     suspend fun fetchMarketInfo(request: Long): Result<MarketInfo>

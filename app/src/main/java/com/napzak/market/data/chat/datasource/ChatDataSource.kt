6149eb7f2b57f30473b7e4de0dd11d7865841b08
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-23 14:01:07 +0900

    [ADD/#95] 트콤

--- app/src/main/java/com/napzak/market/data/chat/datasource/ChatDataSource.kt
+++ app/src/main/java/com/napzak/market/data/chat/datasource/ChatDataSource.kt
@@ -6,7 +6,7 @@ import com.napzak.market.data.chat.service.ChatService
 import javax.inject.Inject
 
 class ChatDataSource @Inject constructor(
-    private val service: ChatService
+    private val service: ChatService,
 ) {
     suspend fun getChatInfo(productId: Long): BaseResponse<ChatInfoResponse> {
         return service.getChatInfo(productId)
--- app/src/main/java/com/napzak/market/data/chat/repositoryimpl/ChatRepositoryImpl.kt
+++ app/src/main/java/com/napzak/market/data/chat/repositoryimpl/ChatRepositoryImpl.kt
@@ -7,7 +7,7 @@ import com.napzak.market.domain.chat.repository.ChatRepository
 import javax.inject.Inject
 
 class ChatRepositoryImpl @Inject constructor(
-    private val dataSource: ChatDataSource
+    private val dataSource: ChatDataSource,
 ) : ChatRepository {
     override suspend fun fetchChatInfo(productId: Long): Result<ChatInfo> = runCatching {
         val response = dataSource.getChatInfo(productId)

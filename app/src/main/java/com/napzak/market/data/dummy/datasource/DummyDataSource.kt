134c7b1300e2144e37e682941980efade409b828
boiledegg <tjrwns2163@naver.com>
2025-01-24 00:45:04 +0900

    [DEL/#111] 더미 삭제

--- app/src/main/java/com/napzak/market/data/dummy/datasource/DummyDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.napzak.market.data.dummy.datasource
-
-import com.napzak.market.data.dummy.dto.GetUserListResponse
-
-interface DummyDataSource {
-    suspend fun getDummyUserList(page: Int): GetUserListResponse
-}
--- app/src/main/java/com/napzak/market/domain/dummy/repository/DummyRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.napzak.market.domain.dummy.repository
-
-import com.napzak.market.domain.dummy.model.DummyUser
-
-interface DummyRepository {
-    suspend fun fetchDummyUserList(page: Int): Result<List<DummyUser>>
-}

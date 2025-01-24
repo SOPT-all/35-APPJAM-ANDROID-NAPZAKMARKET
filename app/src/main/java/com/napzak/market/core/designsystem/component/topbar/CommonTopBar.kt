515b31c08455b387e2554459fd11f39a6b85c73e
jangsjw <wkdwodnjs1999@gmail.com>
2025-01-24 16:05:21 +0900

    [DEL/#118] import 제거

--- app/src/main/java/com/napzak/market/core/designsystem/component/topbar/CommonTopBar.kt
+++ app/src/main/java/com/napzak/market/core/designsystem/component/topbar/CommonTopBar.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable

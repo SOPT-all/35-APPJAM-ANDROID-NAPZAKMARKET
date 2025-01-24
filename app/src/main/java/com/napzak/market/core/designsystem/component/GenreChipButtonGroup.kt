1bd4018f31568e7b75ea0954622b6095c329b1e8
chrin05 <rinring105@gmail.com>
2025-01-24 22:45:30 +0900

    [REFACTOR/#131] 불필요한 import 삭제

--- app/src/main/java/com/napzak/market/core/designsystem/component/GenreChipButtonGroup.kt
+++ app/src/main/java/com/napzak/market/core/designsystem/component/GenreChipButtonGroup.kt
@@ -8,16 +8,11 @@ import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
